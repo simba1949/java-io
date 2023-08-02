ee066c71dec752d2be462fccce5377d9c902b278
anthony <anthony1949@zto.com>
2023-08-02 14:58:14 +0800

    style：使用 System.out.println 替换 log.info 打印日志，用于不依赖于外部项目即可运行

--- java-io-start/src/main/java/top/simba1949/file/GetFileInfoApplication.java
+++ java-io-start/src/main/java/top/simba1949/file/GetFileInfoApplication.java
@@ -1,47 +1,57 @@
 package top.simba1949.file;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author anthony
  * @version 2023/7/26 22:25
  */
 public class GetFileInfoApplication {
-	public static final Logger log = LoggerFactory.getLogger(GetFileInfoApplication.class);
 
 	/**
 	 * 获取文件和目录信息
 	 *
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String filePath = "D:/Program Files/file.txt";
 		File file = new File(filePath);
 		// 获取文件名称
 		String name = file.getName();
-		log.info("获取文件名：{}", name);
+		System.out.println("获取文件名：" + name);
 
-		// 获取文件路径
-		String path = file.getPath();
-		log.info("获取文件路径：{}", path);
-
-		// 获取文件绝对路径
+		// 获取文件绝对路径（如果文件对象路径是绝对路径，返回绝对路径，如果不是绝对路径，拼接相对路径返回）
 		String absolutePath = file.getAbsolutePath();
-		log.info("获取文件绝对路径：{}", absolutePath);
+		System.out.println("获取文件绝对路径-getAbsolutePath：" + absolutePath);
+		// getCanonicalPath 规范路径名字符串。规范路径名既是绝对的，也是唯一的【类似于 java.io.Path.toRealPath】
+		String canonicalPath = file.getCanonicalPath();
+		System.out.println("获取文件绝对路径-getCanonicalPath：" + canonicalPath);
 
 		// 获取父级文件路径
 		String parent = file.getParent();
-		log.info("获取父级文件路径：{}", parent);
+		System.out.println("获取父级文件路径：" + parent);
 
 		// 获取文件字节数
 		long length = file.length();
-		log.info("获取到文件字节数：{}", length);
+		System.out.println("获取到文件字节数：" + length);
+
+		// 获取文件所在磁盘的总容量字节数
+		long totalSpace = file.getTotalSpace();
+		System.out.println("获取文件所在磁盘的总容量字节数： " + totalSpace);
+		// 获取文件所在磁盘的已使用容量字节数
+		long usableSpace = file.getUsableSpace();
+		System.out.println("获取文件所在磁盘的已使用容量字节数： " + usableSpace);
+		// 获取文件所在磁盘的未使用容量字节数
+		long freeSpace = file.getFreeSpace();
+		System.out.println("获取文件所在磁盘的未使用容量字节数： " + freeSpace);
 
 		// 获取最后一次修改文件的时间
 		long lastedModified = file.lastModified();
-		log.info("获取到文件最后一次修改时间：{}", lastedModified);
+		System.out.println("获取到文件最后一次修改时间：" + lastedModified);
+
+		// 获取文件路径（可能是绝对路径，也可能是相对路径）
+		String path = file.getPath();
+		System.out.println("获取文件路径：" + path);
 	}
 }
--- java-io-start/src/main/java/top/simba1949/file/JudgeFileApplication.java
+++ java-io-start/src/main/java/top/simba1949/file/JudgeFileApplication.java
@@ -1,8 +1,5 @@
 package top.simba1949.file;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 
 /**
@@ -10,32 +7,37 @@ import java.io.File;
  * @version 2023/7/26 23:05
  */
 public class JudgeFileApplication {
-	public static final Logger log = LoggerFactory.getLogger(JudgeFileApplication.class);
 
 	public static void main(String[] args) {
 		String filePath = "D:\\Program Files\\file.txt";
 		File file = new File(filePath);
 
+		// 判断路径是否是绝对路径，Unix 下以 / 开头，windows 下以盘符\\或者\\\\开头
+		boolean absoluteFlag = file.isAbsolute();
+		System.out.println("判断文件路径是否是绝对路径：" + absoluteFlag);
+		File file2 = new File("./../file.txt");
+		boolean absoluteFlag2 = file2.isAbsolute();
+		System.out.println("判断文件路径是否是绝对路径-2：" + absoluteFlag2);
 
 		boolean existsFlag = file.exists();
-		log.info("判断文件对象是否存在：{}", existsFlag);
-
-		boolean directoryFlag = file.isDirectory();
-		log.info("判断文件对象是否是文件夹：{}", directoryFlag);
-
-		boolean fileFlag = file.isFile();
-		log.info("判断文件对象是否是文件：{}", fileFlag);
-
-		boolean readFlag = file.canRead();
-		log.info("判断文件是否可读：{}", readFlag);
-
-		boolean writeFlag = file.canWrite();
-		log.info("判断文件是否可写：{}", writeFlag);
-
-		boolean canExecuteFlag = file.canExecute();
-		log.info("判断文件是否可执行：{}", canExecuteFlag);
+		System.out.println("判断文件对象是否存在：" + existsFlag);
 
 		boolean hiddenFlag = file.isHidden();
-		log.info("判断文件是否隐藏：{}", hiddenFlag);
+		System.out.println("判断文件是否隐藏：" + hiddenFlag);
+
+		boolean directoryFlag = file.isDirectory();
+		System.out.println("判断文件对象是否是文件夹：" + directoryFlag);
+
+		boolean fileFlag = file.isFile();
+		System.out.println("判断文件对象是否是文件：" + fileFlag);
+
+		boolean readFlag = file.canRead();
+		System.out.println("判断文件是否可读：" + readFlag);
+
+		boolean writeFlag = file.canWrite();
+		System.out.println("判断文件是否可写：" + writeFlag);
+
+		boolean canExecuteFlag = file.canExecute();
+		System.out.println("判断文件是否可执行：" + canExecuteFlag);
 	}
 }
