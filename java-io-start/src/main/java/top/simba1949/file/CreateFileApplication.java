53ad726255134023a6350e3192e5b5369b62c74e
anthony <anthony1949@zto.com>
2023-08-02 14:16:48 +0800

    style：使用 System.out.println 替换 log.info 打印日志，用于不依赖于外部项目即可运行

--- java-io-start/src/main/java/top/simba1949/file/CreateFileApplication.java
+++ java-io-start/src/main/java/top/simba1949/file/CreateFileApplication.java
@@ -1,8 +1,5 @@
 package top.simba1949.file;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -17,121 +14,130 @@ import java.io.IOException;
  * @version 2023/7/26 21:27
  */
 public class CreateFileApplication {
-	public static final Logger log = LoggerFactory.getLogger(CreateFileApplication.class);
 
-	/**
-	 * 基于文件路径，创建文件对象
-	 * <p>
-	 * Windows
-	 * 绝对路径：
-	 * - D:\\Program Files\\file.txt
-	 * - D:/Program Files/file.txt
-	 * 相对路径：（如果是在 main 包下相对路径是基于整个项目下的路径；如果是在 test 包下相对路径是基于该模块下的路径）
-	 * - ./../file.txt
-	 * - .\..\file.txt
-	 * </p>
-	 * <p>
-	 * Linux
-	 * 绝对路径：/data/file.txt
-	 * 相对路径：./../file.text
-	 * </p>
-	 */
-	public static void main(String[] args) throws IOException {
-		// 绝对路径
-		// absolutePath1();
-		// absolutePath2();
+    /**
+     * 基于文件路径，创建文件对象
+     * <p>
+     * Windows
+     * 绝对路径：
+     * - D:\\Program Files\\file.txt
+     * - D:/Program Files/file.txt
+     * 相对路径：（如果是在 main 包下相对路径是基于整个项目下的路径；如果是在 test 包下相对路径是基于该模块下的路径）
+     * - .\\..\\file.txt
+     * - ./../file.txt
+     * </p>
+     * <p>
+     * Linux
+     * 绝对路径：/data/file.txt
+     * 相对路径：./../file.text
+     * </p>
+     */
+    public static void main(String[] args) throws IOException {
+        // 绝对路径
+        // absolutePath1();
+        // absolutePath2();
 
-		// 相对路径
-		// relativePath1();
-		// relativePath2();
+        // 相对路径
+        // relativePath1();
+        // relativePath2();
 
-		netPath();
-	}
+        netPath();
+    }
 
-	/**
-	 * 基于网络资源，创建 File 对象；
-	 */
-	public static void netPath() {
-		// 网络资源
-		String netPath = "https://repo1.maven.org/maven2/org/junit/jupiter/junit-jupiter/5.10.0/junit-jupiter-5.10.0.jar";
-		File netPathFile = new File(netPath);
+    /**
+     * 基于网络资源，创建 File 对象；
+     */
+    public static void netPath() throws IOException {
+        // 网络资源
+        String netPath = "https://repo1.maven.org/maven2/ch/qos/logback/logback-classic/1.4.8/logback-classic-1.4.8.jar";
+        File netPathFile = new File(netPath);
 
-		// 输出网络文件名
-		log.info("获取到的网络资源文件名：{}", netPathFile.getName());
-	}
+        // 输出网络文件名
+        System.out.println("获取到的网络资源文件名：" + netPathFile.getName());
+    }
 
-	public static void absolutePath1() throws IOException {
-		// 绝对路径
-		String absolutePath = "D:\\Program Files\\file.txt";
-		File absolutePathFile = new File(absolutePath);
+    /**
+     * 绝对路径：D:\\Program Files\\file.txt
+     *
+     * @throws IOException
+     */
+    public static void absolutePath1() throws IOException {
+        // 绝对路径
+        String absolutePath = "D:\\Program Files\\file.txt";
+        File absolutePathFile = new File(absolutePath);
 
-		// 输出绝对路径
-		log.info("当前文件对象的绝对路径是：{}", absolutePathFile.toPath().toAbsolutePath());
+        // 输出绝对路径
+        // getCanonicalPath 规范路径名字符串。规范路径名既是绝对的，也是唯一的【类似于 java.io.Path.toRealPath】
+        System.out.println("当前文件对象的绝对路径是：" + absolutePathFile.getCanonicalPath());
 
-		if (absolutePathFile.createNewFile()) {
-			log.info("新文件创建完毕！");
-		} else {
-			log.info("文件已存在，无需创建！");
-		}
-	}
+        // 在系统中创建新文件
+        if (absolutePathFile.createNewFile()) {
+            System.out.println("新文件创建完毕！");
+        } else {
+            System.out.println("文件已存在，无需创建！");
+        }
+    }
 
-	/**
-	 * 绝对路径2
-	 *
-	 * @throws IOException
-	 */
-	public static void absolutePath2() throws IOException {
-		// 绝对路径
-		String absolutePath = "D:/Program Files/file.txt";
-		File absolutePathFile = new File(absolutePath);
+    /**
+     * 绝对路径：D:/Program Files/file.txt
+     *
+     * @throws IOException
+     */
+    public static void absolutePath2() throws IOException {
+        // 绝对路径
+        String absolutePath = "D:/Program Files/file.txt";
+        File absolutePathFile = new File(absolutePath);
 
-		// 输出绝对路径
-		log.info("当前文件对象的绝对路径是：{}", absolutePathFile.toPath().toAbsolutePath());
+        // 输出绝对路径
+        // getCanonicalPath 规范路径名字符串。规范路径名既是绝对的，也是唯一的【类似于 java.io.Path.toRealPath】
+        System.out.println("当前文件对象的绝对路径是：" + absolutePathFile.getCanonicalPath());
 
-		if (absolutePathFile.createNewFile()) {
-			log.info("新文件创建完毕！");
-		} else {
-			log.info("文件已存在，无需创建！");
-		}
-	}
+        if (absolutePathFile.createNewFile()) {
+            System.out.println("新文件创建完毕！");
+        } else {
+            System.out.println("文件已存在，无需创建！");
+        }
+    }
 
-	/**
-	 * 相对路径1
-	 *
-	 * @throws IOException
-	 */
-	public static void relativePath1() throws IOException {
-		// 相对路径
-		String relativePath = "./../file.txt";
-		File relativePathFile = new File(relativePath);
+    /**
+     * 相对路径：.\\..\\file.txt
+     *
+     * @throws IOException
+     */
+    public static void relativePath1() throws IOException {
+        // 相对路径：
+        String relativePath = ".\\..\\file.txt";
+        File relativePathFile = new File(relativePath);
 
-		// 输出绝对路径
-		log.info("当前文件对象的绝对路径是：{}", relativePathFile.toPath().toAbsolutePath());
+        // 输出绝对路径
+        // getCanonicalPath 规范路径名字符串。规范路径名既是绝对的，也是唯一的【类似于 java.io.Path.toRealPath】
+        System.out.println("当前文件对象的绝对路径是：" + relativePathFile.getCanonicalPath());
 
-		if (relativePathFile.createNewFile()) {
-			log.info("新文件创建完毕！");
-		} else {
-			log.info("文件已存在，无需创建！");
-		}
-	}
+        if (relativePathFile.createNewFile()) {
+            System.out.println("新文件创建完毕！");
+        } else {
+            System.out.println("文件已存在，无需创建！");
+        }
+    }
 
-	/**
-	 * 相对路径2
-	 *
-	 * @throws IOException
-	 */
-	public static void relativePath2() throws IOException {
-		// 相对路径：
-		String relativePath = ".\\..\\file.txt";
-		File relativePathFile = new File(relativePath);
+    /**
+     * 相对路径：./../file.txt
+     *
+     * @throws IOException
+     */
+    public static void relativePath2() throws IOException {
+        // 相对路径
+        String relativePath = "./../file.txt";
+        File relativePathFile = new File(relativePath);
 
-		// 输出绝对路径
-		log.info("当前文件对象的绝对路径是：{}", relativePathFile.toPath().toAbsolutePath());
+        // 输出绝对路径
+        // getCanonicalPath 规范路径名字符串。规范路径名既是绝对的，也是唯一的【类似于 java.io.Path.toRealPath】
+        System.out.println("当前文件对象的绝对路径是：" + relativePathFile.getCanonicalPath());
 
-		if (relativePathFile.createNewFile()) {
-			log.info("新文件创建完毕！");
-		} else {
-			log.info("文件已存在，无需创建！");
-		}
-	}
+        if (relativePathFile.createNewFile()) {
+            System.out.println("新文件创建完毕！");
+        } else {
+            System.out.println("文件已存在，无需创建！");
+        }
+    }
 }
