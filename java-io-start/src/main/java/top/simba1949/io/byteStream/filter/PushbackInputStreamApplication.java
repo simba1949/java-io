320779c40c0ca780507c931656f99ac26fe74f05
simba1949 <simba1949@outlook.com>
2023-08-04 21:15:36 +0800

    feature：PushbackInputStream 的使用

--- java-io-start/src/main/java/top/simba1949/io/byteStream/filter/PushbackInputStreamApplication.java
+++ java-io-start/src/main/java/top/simba1949/io/byteStream/filter/PushbackInputStreamApplication.java
@@ -10,51 +10,52 @@ import java.io.PushbackInputStream;
  * @date 2023/8/4
  */
 public class PushbackInputStreamApplication {
-    public static void main(String[] args) {
-        input();
-    }
+	public static void main(String[] args) {
+		input();
+	}
 
-    /**
-     * 使用推回输入流
-     */
-    public static void input() {
-        String filePath = "./java-io-start/src/main/resources/file/byte/filter/PushbackInputStream";
-        File file = new File(filePath);
+	/**
+	 * 使用推回输入流
+	 */
+	public static void input() {
+		String filePath = "./java-io-start/src/main/resources/file/byte/filter/PushbackInputStream";
+		File file = new File(filePath);
 
-        PushbackInputStream pis = null;
-        try {
-            pis = new PushbackInputStream(new FileInputStream(file));
+		PushbackInputStream pis = null;
+		try {
+			pis = new PushbackInputStream(new FileInputStream(file));
+			// 用于表示是否是上一次读取的 \n 数据，如果是打印出来；如果不是，说明是本次循环的数据，推回输入流中，进入下一循环打印
+			boolean lastReadFlag = false;
+			for (int i = 0; i < 6; i++) {
 
-            for (int i = 0; i < 9; i++) {
-                int readByte = -1;
-                boolean firstPerFlag = true;
-                while ((readByte = pis.read()) != -1) {
-                    char readChar = (char) readByte;
-                    if (!firstPerFlag) {
-                        pis.skip(1);
-                    }
+				int readByte = -1;
+				while ((readByte = pis.read()) != -1) {
+					char readChar = (char) readByte;
+					if ('\n' == readChar && !lastReadFlag) {
+						// 如果读取的字符是 \n，回退该字符到缓冲流中
+						pis.unread(readByte);
+						lastReadFlag = true;
+						// 打破当前循环，进入下一循环
+						break;
+					} else {
+						lastReadFlag = false;
+						System.out.print(readChar);
+					}
+				}
 
-                    if ('\n' == readChar && firstPerFlag) {
-                        // 如果读取的字符是 I，回退该字符到缓冲流中
-                        pis.unread(readByte);
-                        firstPerFlag = false;
-                        break;
-                    } else {
-                        System.out.print(readChar);
-                    }
-                }
-                System.out.println("-------------------------");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (null != pis) {
-                try {
-                    pis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+				// 用来分割每一循环
+				System.out.println("");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (null != pis) {
+				try {
+					pis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
