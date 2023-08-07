76a8b655ecfff069b21f4e70836f6c524f166e9f
anthony <anthony1949@zto.com>
2023-08-07 18:27:48 +0800

    feature：PrintWriter 的基本使用

--- java-io-start/src/main/java/top/simba1949/io/characterStream/print/PrintWriterApplication.java
+++ java-io-start/src/main/java/top/simba1949/io/characterStream/print/PrintWriterApplication.java
@@ -11,15 +11,20 @@ import java.io.PrintWriter;
  */
 public class PrintWriterApplication {
     public static void main(String[] args) throws FileNotFoundException {
-        printWriter();
+        printToFile();
     }
 
-    public static void printWriter() throws FileNotFoundException {
+    /**
+     * 重定向到文件中
+     *
+     * @throws FileNotFoundException
+     */
+    public static void printToFile() throws FileNotFoundException {
         // 创建输出源
-        String writeFilePath = "./java-io-start/src/main/resources/file/PrintWriter";
+        String writeFilePath = "./java-io-start/src/main/resources/file/character/PrintWriter";
 
-        PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(writeFilePath)));
-        printWriter.println("printWriter 流笔记");
+        PrintWriter printWriter = new PrintWriter(new BufferedOutputStream(new FileOutputStream(writeFilePath)), true);
+        printWriter.println("重定向到文件中：君不见黄河之水天上来");
         printWriter.flush();
         printWriter.close();
     }
