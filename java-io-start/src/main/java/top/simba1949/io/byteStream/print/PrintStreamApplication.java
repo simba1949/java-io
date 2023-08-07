bce73081605f77a2a520cfcb2061f56f9c91344f
anthony <anthony1949@zto.com>
2023-08-07 15:58:34 +0800

    feature：PrintStream 的基本使用

--- java-io-start/src/main/java/top/simba1949/io/byteStream/print/PrintStreamApplication.java
+++ java-io-start/src/main/java/top/simba1949/io/byteStream/print/PrintStreamApplication.java
@@ -8,20 +8,36 @@ import java.io.*;
  */
 public class PrintStreamApplication {
     public static void main(String[] args) throws FileNotFoundException {
-        printStream();
+        // 重定向到文件
+        // printToFile();
+        // 重定向到控制台
+        printToConsole();
     }
 
-    public static void printStream() throws FileNotFoundException {
-        String readFilePath = "./java-io-start/src/main/resources/file/PrintStream";
+    /**
+     * 重定向到文件
+     *
+     * @throws FileNotFoundException
+     */
+    public static void printToFile() throws FileNotFoundException {
+        String readFilePath = "./java-io-start/src/main/resources/file/byte/PrintStream";
         File readFile = new File(readFilePath);
+
         PrintStream printStream = new PrintStream(new BufferedOutputStream(new FileOutputStream(readFile)), true);
+
         // 打印流重定向到文件中
         System.setOut(printStream);
-        printStream.println("重定向到文件中");
+        printStream.println("重定向到文件中：君不见黄河之水天上来");
         printStream.close();
+    }
 
+    /**
+     * 重定向到控制台
+     */
+    public static void printToConsole() {
+        PrintStream printStream = new PrintStream(new BufferedOutputStream(new FileOutputStream(FileDescriptor.out)), true);
         // 在重定向到控制台
-        System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(FileDescriptor.out)), true));
-        System.out.println("重定向到控制台");
+        System.setOut(printStream);
+        printStream.println("重定向到控制台");
     }
 }
