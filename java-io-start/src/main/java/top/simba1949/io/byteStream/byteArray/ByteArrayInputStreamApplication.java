25df8d26922c62679a92ea9cd2a96da30f2e41b7
anthony <anthony1949@zto.com>
2023-08-07 15:06:26 +0800

    feature：ByteArrayInputStream 的基本使用

--- java-io-start/src/main/java/top/simba1949/io/byteStream/byteArray/ByteArrayInputStreamApplication.java
+++ java-io-start/src/main/java/top/simba1949/io/byteStream/byteArray/ByteArrayInputStreamApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ByteArrayInputStreamApplication {
     public static void main(String[] args) {
         // ByteArrayInputStream 单字节读取
-        // singleByteRead();
+        singleByteRead();
 
         // ByteArrayInputStream 自定义缓存读取数据
         // flushRead();
@@ -25,18 +25,26 @@ public class ByteArrayInputStreamApplication {
         // 创建源
         byte[] readStrBytes = readStr.getBytes();
 
-        // 选择流
-        ByteArrayInputStream is = new ByteArrayInputStream(readStrBytes);
-        // 每次读取的字节信息
-        int readByte = -1;
-        while ((readByte = is.read()) != -1) {
-            System.out.print((char) readByte);
-        }
+        ByteArrayInputStream bais = null;
         try {
-            // 关闭流
-            is.close();
-        } catch (IOException e) {
+            // 选择流
+            bais = new ByteArrayInputStream(readStrBytes);
+
+            // 每次读取的字节信息
+            int readByte = -1;
+            while ((readByte = bais.read()) != -1) {
+                System.out.print((char) readByte);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (null != bais) {
+                try {
+                    bais.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -50,22 +58,25 @@ public class ByteArrayInputStreamApplication {
         byte[] readStrBytes = readStr.getBytes();
 
         // 选择流
-        ByteArrayInputStream is = new ByteArrayInputStream(readStrBytes);
+        ByteArrayInputStream bais = null;
         try {
+            bais = new ByteArrayInputStream(readStrBytes);
             // 自定义缓冲区
             byte[] flush = new byte[2];
             // 每次读取多少字节数，len 表示每次读取字节数的实际大小，内容最后一次读取完成后，还有进行一次读取，但是读取内容为空，返回-1
             int len = -1;
-            while ((len = is.read(flush)) != -1) {
+            while ((len = bais.read(flush)) != -1) {
                 System.out.print(new String(flush, 0, len));
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (null != bais) {
+                try {
+                    bais.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
